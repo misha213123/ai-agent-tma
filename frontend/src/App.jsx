548a8f57dedcf051045a79a607b535c40f08ff1d
misha213123 <misha0228050@gmail.com>
2026-05-25 23:47:18 +0200

    setup frontend vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,17 @@
-# App.jsx
+export default function App() {
+  return (
+    <div
+      style={{
+        background: "#07070a",
+        color: "white",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "32px"
+      }}
+    >
+      AI AGENT TMA 🚀
+    </div>
+  );
+}
